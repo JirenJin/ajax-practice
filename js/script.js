2b86e7f050644c71e01110b0684e81bf449d6f29
Jiren Jin <jinjiren@foxmail.com>
2016-07-14 11:27:31 +0900

    feat: Add nytimes articles according to the input city

--- js/script.js
+++ js/script.js
@@ -14,9 +14,30 @@ function loadData() {
     // load streetview
     var street = $('#street').val();
     var city = $('#city').val();
+    var address = street + ', ' + city;
+
+    $greeting.text('So, you want to live at ' + address + '?');
+
     var backgroundImage = '<img class="bgimg" src="%data%">';
-    var imagePath = "http://maps.googleapis.com/maps/api/streetview?size=600x400&location= " + street + ", " + city;
+    var imagePath = 'http://maps.googleapis.com/maps/api/streetview?size=600x400&location= ' + street + ', ' + city;
     $body.append(backgroundImage.replace('%data%', imagePath));
+
+
+    // load nytimes
+    var url = 'https://api.nytimes.com/svc/search/v2/articlesearch.json?q=' + city;
+
+    $.getJSON(url, function(data){
+      console.log(data);
+      $.each(data, function(){
+        data.response.docs.forEach(function(doc) {
+          var template = '<li class="article"> <a href="%url%">%title%</a> <p> %leadParagraphs%</p> </li>'
+          template = template.replace('%url%', doc.web_url);
+          template = template.replace('%title%', doc.headline.main);
+          template = template.replace('%leadParagraphs%', doc.lead_paragraph);
+          $('#nytimes-articles').append(template);
+        });
+      });
+    });
 
     return false;
 };
